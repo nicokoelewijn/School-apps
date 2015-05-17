f22d024b1fdd544c68627f4e36588082ed462d02
nicokoelewijn <nico.koelewijn@gmail.com>
2015-05-18 01:38:44 +0200

    added SnobChat app

---
+++ SnobChat/SnobChat/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  SnobChat
+//
+//  Created by Nico Koelewijn on 17/05/15.
+//  Copyright (c) 2015 Nico Koelewijn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var imageView:UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "SnobChat"
+        
+        self.imageView = UIImageView(frame: self.view.bounds)
+        self.view.addSubview(self.imageView)
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Camera, target: self, action: "cameraButtonTouched")
+    }
+    
+    func cameraButtonTouched(){
+        println("Camera button touched")
+        if UIImagePickerController.isSourceTypeAvailable(.Camera){
+            println("camer beschikbaar")
+            
+            var mediatypes = UIImagePickerController.availableMediaTypesForSourceType(UIImagePickerControllerSourceType.Camera)
+            println(mediatypes)
+            
+            let imagepicker = UIImagePickerController()
+            imagepicker.sourceType = UIImagePickerControllerSourceType.Camera
+            imagepicker.mediaTypes = mediatypes!
+            imagepicker.delegate = self
+            
+            self.presentViewController(imagepicker, animated: true, completion: nil)
+            
+        } else {
+            println("camera niet beschikbaar")
+            
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+
+        self.imageView.image = image
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
