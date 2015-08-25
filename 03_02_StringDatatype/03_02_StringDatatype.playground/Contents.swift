a5605b7ce012ff513f7e10e6fa7023e6acd06fc0
nicokoelewijn <nico.koelewijn@gmail.com>
2015-08-25 23:16:37 +0200

    added stuff

---
+++ 03_02_StringDatatype/03_02_StringDatatype.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var hello:String = "Hello, playground"
+//hello = 42 error
+
+println(hello)
+
+
+var nameWithoutValue:String
---
+++ 03_02_Variable/Variable.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var hello = "Hello my favorite Deviners"
+
+println(hello)
